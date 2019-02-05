bc584f521f08ca6e95a29577133c5ba50d646091
yv71 <33083095+yv71@users.noreply.github.com>
2019-02-05 10:39:43 +0100

    Hello

--- BotAnnick/src/main/java/test.java
+++ BotAnnick/src/main/java/test.java
@@ -1,25 +1,36 @@
+import Listeners.Readylistenener;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.Event;
+import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.hooks.EventListener;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
 import javax.security.auth.login.LoginException;
 
-public class test {
+public class test  {
     public static void main(String args[]){
-        Twitter twitter = TwitterFactory.getSingleton();
         JDABuilder jda = new JDABuilder(AccountType.BOT);
         try {
             jda.setToken(Secret.token).build();
+            jda.addEventListener(new Readylistenener());
 
         } catch (LoginException e) {
             e.printStackTrace();
         }
+
+        /**Twitter twitter = TwitterFactory.getSingleton();
         try {
             twitter.updateStatus("Salut les boyz");
+            twitter.directMessages().sendDirectMessage("@Yoann_Beelzed", "Coucouu");
         } catch (TwitterException e) {
             e.printStackTrace();
-        }
+        }**/
     }
+
+
+
+
 }
